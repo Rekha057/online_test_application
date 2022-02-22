83cdebe39394b5add6a88f33371878a439ec7c03
rekha <rekhasamanvitha@gmail.com>
2022-02-23 02:34:04 +0530

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { QuizListComponent } from './components/quiz-list/quiz-list/quiz-list.component';
+import { QuizComponent } from './components/quiz/quiz/quiz.component';
+import { RouterModule, Routes } from '@angular/router'
+import { QuizService } from './services/quiz.service';
 
+const routes: Routes = [
+  { path: 'quiz/:quizName', component: QuizComponent },
+  { path: 'quiz', component: QuizListComponent },
+  { path: '', redirectTo: '/quiz', pathMatch: 'full' },
+  { path: '**', redirectTo: '/quiz', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizListComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [QuizService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
